c83ec33755ed95af28fc329142347942424de6ef
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-08-04 23:52:25 +0530

    Routes added

--- utils/User Based/AuthenticatedUserActions.js
+++ utils/User Based/AuthenticatedUserActions.js
@@ -52,7 +52,48 @@ async function inviteUser(phoneNumber, context = "add") {
         return { success: false, message: error.message || "An error occurred while inviting the user" };
     }
 }
+async function fetchInvite(UID) {
+    try {
+        // Check if onboarding is still happening
+        if (isOnboarding) {
+            console.error("User is still onboarding");
+            return;
+        }
 
+        // Fetch jwt from cache
+        const jwt = Cache.getString("jwt");
+        if (!jwt) {
+            console.error("No jwt in the cache");
+            return;
+        }
+
+        // Prepare request url
+        const url = endpoints["/invitations/fetch"];
+
+        // Prepare axios configuration
+        const axiosConfig = {
+            headers: {
+                Authorization: 'Bearer ' + jwt
+            },
+            params: {
+               uid:UID
+            }
+        };
+
+        // Send get request
+        const response = await AxiosSigned.get(url, axiosConfig);
+
+        if (response.data.success) {
+            return { success: true, data: response.data };
+        } else {
+            return { success: false, message: response.data.message || "An error occurred while fetching the invite" };
+        }
+
+    } catch (error) {
+        console.error(error);
+        return { success: false, message: error.message || "An error occurred while fetching the invitation" };
+    }
+}
 async function OnPollReveal(messageUID,answerFirstLetter) {
     const QueryString = { messageUID: messageUID, answerFirstLetter: answerFirstLetter };
     const endpoint = endpoints["/OnPollRevealed"];
@@ -77,6 +118,26 @@ async function DispatchVote(polls) {
     return res;
 }
 
+async function RequestDeletion() {
+    const jwt = Cache.getString("jwt");
+    if (!jwt) {
+        console.error("No jwt in the cache");
+        return;
+    }
+    const url = endpoints["/requestDeletion"];
+    const res = await AxiosSigned.post(url, jwt, null, null);
+    return res;
+}
+async function DisableDeletion() {
+    const jwt = Cache.getString("jwt");
+    if (!jwt) {
+        console.error("No jwt in the cache");
+        return;
+    }
+    const url = endpoints["/disableDeletion"];
+    const res = await AxiosSigned.post(url, jwt, null, null);
+    return res;
+}
 async function FetchPollsNow() {
     const endpoint = endpoints["/fetchPollsNow"];
     const jwt = Cache.getString("jwt");
@@ -88,5 +149,8 @@ exports.module = {inviteUser,
     OnPollReveal,
     ReadInbox,
     DispatchVote,
-    FetchPollsNow
+    FetchPollsNow,
+    fetchInvite,
+    DisableDeletion,
+    RequestDeletion
 }
